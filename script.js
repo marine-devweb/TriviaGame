9c8184af0176a2ccbc331171f6497686e92c6e18
marine-devweb <marine.devweb@gmail.com>
2024-02-07 15:16:27 +0100

    Ajout de la fonction displayQuestion()

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-const question = "What planet is known as the 'Red Planet'?";
-const choices = ["Earth", "Mars", "Jupiter", "Saturn"];
+const question = "Quelle planète est connue sous le nom de « Planète rouge » ?";
+const choices = ["Terre", "Mars", "Jupiter", "Saturne"];
 const correctAnswer = "Mars";
+
+// Utilisée pour afficher une question et une liste de choix
+function displayQuestion() {
+    document.getElementById('question').innerHTML = question;
+  
+    for (let i = 0; i < 4; i++) {
+    const btn = document.getElementById(`choice${i+1}`);
+    btn.innerHTML = choices[i];
+    btn.value = choices[i];
+    }
+}
+displayQuestion();
